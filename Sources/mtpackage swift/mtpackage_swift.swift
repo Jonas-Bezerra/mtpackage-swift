3ededa80c808b2ac201bb881edb1ef69d688a202
jonas.junior <jonas.junior@valemobi.com.br>
2022-06-08 10:54:57 -0300

    organizando a classe LibView

--- Sources/mtpackage swift/mtpackage_swift.swift
+++ Sources/mtpackage swift/mtpackage_swift.swift
@@ -1,6 +1,8 @@
 import UIKit
 
-public class lbView: UIView {
+public class LibView: UIView {
+    public static let staticLet: String = "Eu sou uma static let da lib"
+    
     public init() {
         super.init(frame: .zero)
         backgroundColor = .purple
@@ -9,8 +11,4 @@ public class lbView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
-
-public struct estruturaDaLib {
-    static let staticLet: String = "Eu sou uma static let da lib"
 }
