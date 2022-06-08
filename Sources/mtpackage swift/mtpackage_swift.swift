e24d5957a17015987d272c658b6828710f665059
jonas.junior <jonas.junior@valemobi.com.br>
2022-06-08 10:32:51 -0300

    testando integração

--- Sources/mtpackage swift/mtpackage_swift.swift
+++ Sources/mtpackage swift/mtpackage_swift.swift
@@ -1,6 +1,29 @@
+import UIKit
+
+public class libViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
+
+public class lbView: UIView {
+    public init() {
+        super.init(frame: .zero)
+        backgroundColor = .purple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
 public struct mtpackage_swift {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print(text)
     }
 }
